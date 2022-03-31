03564b68c43daebe19081c6dd4190e7e617acf5b
DanielRoncallo <roncallodaniel@hotmail.com>
2022-03-31 08:38:47 -0500

    fix

--- lib/main_app1.dart
+++ lib/main_app1.dart
@@ -3,6 +3,7 @@ import 'package:flavorsexample/main_Common.dart';
 import 'package:flavorsexample/resource/display_strings_app1.dart';
 import 'package:flutter/material.dart';
 
+//app 1
 void main() {
   var configuredApp = AppConfig(
     appDisplayName: "App 1",
